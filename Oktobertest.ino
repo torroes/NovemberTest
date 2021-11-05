83dd7a3d1d04e358ff48fdba5eb651bce5ece827
torroes <torroes@hedmark.org>
2021-11-05 09:10:33 +0100

    Update Oktobertest.ino

--- Oktobertest.ino
+++ Oktobertest.ino
@@ -2,8 +2,8 @@
 gjør endringer i koden slik at vi får en enkel alarm sentra med en bryter som aktiverer alarmen 
 og en PIR som utløser alarmen
 a) Hvis det går en alarm skal en lysdiode lyse
-b) hvis alarsentralen er av skal en grønn lysdiode lyse og vis, detektoren utløser da ikke en alarm.
-   hvis alarmsentralen er aktivert med bryteren skal detektoren kunne utløse en alarm.
+b) hvis alarsentralen er av skal en grønn lysdiode lyse detektoren utløser da ikke en alarm.
+   hvis alarmsentralen er aktivert med bryteren skal detektoren kunne utløse en alarm og en rød lysdiode skal lyse.
 c) hvis alarsentralen er av skal en grønn lysdiode lyse, detektoren utløser da ikke en alarm.
    hvis alarsentralen er på skal en gul lysdiode lyse, detektoren utløser da en alarm 
    og en rød lysdiode skal blinke 10 ganger.
